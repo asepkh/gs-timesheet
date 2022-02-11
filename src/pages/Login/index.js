37c51087f5a7001cb895fd5359097e985608d3a6
mhdjanuar <mhdjanuar.17@gmail.com>
2022-02-11 15:02:00 +0700

    created - login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,48 @@
 import React from "react";
+import { Form, Input, Button, Checkbox, Card } from 'antd';
 
 const Login = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
   return (
-    <h2 style={{ textAlign: "center", fontWeight: "bolder", marginTop: 200 }}>
-      LOGIN PAGE
-    </h2>
+    <div style={{display: 'flex', justifyContent: 'center', paddingTop: '5rem'}}>
+      <Card title="Login TimeSheet Gudang Solusi" bordered={false} style={{ width: 500 }}>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 };
 
