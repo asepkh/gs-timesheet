6b90481dde41ba4e9562d910a8b9a8d23fe17959
mhdjanuar <mhdjanuar.17@gmail.com>
2022-02-16 01:06:53 +0700

    updated - menu biodata, add upload image

--- src/pages/Biodata/index.js
+++ src/pages/Biodata/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Grid, Row, Col, Form, Input, Button } from 'antd';
+import { Grid, Row, Col, Form, Input, Button, Upload } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 
 const { useBreakpoint } = Grid;
 
@@ -15,24 +16,44 @@ const Biodata = () => {
     console.log('Failed:', errorInfo);
   };
 
+  const { loading, imageUrl } = React.useState('');
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Foto Profil</div>
+    </div>
+  );
+
   return (
     <div>
-      <h2 style={{ fontWeight: 700, textAlign: "center" }}>
-        BIODATA 
-      </h2>
       <Row>
         <Col xs={{ span: 24, offset: 1 }} md={{ span: 6 }}>
-          Col
+          <Upload
+            name="avatar"
+            listType="picture-card"
+            className="avatar-uploader"
+            showUploadList={false}
+          >
+            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+          </Upload>
         </Col>
         <Col xs={{ span: 24, offset: 1 }} md={{ span: 16 }}>
           <Form
             name="basic"
-            wrapperCol={{span: 24}}
+            labelCol={{lg: {span: 5}, md: {span: 7}, sm: {span: 5}}}
+            wrapperCol={{lg: {span: 24, offset: 1}, md: {span: 24, offset: 1}, sm: {span: 16, offset: 1}}}
             initialValues={{ remember: true }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
             autoComplete="off"
           >
+            <Form.Item
+              label="Nama"
+              name="name"
+              rules={[{ required: true, message: 'Please input your name!' }]}
+            >
+              <Input />
+            </Form.Item>
             <Form.Item
               label="Username"
               name="username"
@@ -49,9 +70,12 @@ const Biodata = () => {
               <Input.Password />
             </Form.Item>
 
-            <Form.Item>
+            <Form.Item wrapperCol={{lg: {span: 24, offset: 6}, 
+              md: {span: 24, offset: 8},
+              sm: {span: 24, offset: 6}
+              }}>
               <Button type="primary" htmlType="submit">
-                Submit
+                Simpan
               </Button>
             </Form.Item>
           </Form>
