bfad3af272f7393368df323db921b74ee16be0f4
mhdjanuar <mhdjanuar.17@gmail.com>
2022-02-15 09:46:32 +0700

    created - users page add table and form input

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,12 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { Table, Button, Modal, Form, Input } from 'antd';
 
 const Users = () => {
+
+  const [visible, setVisible] = useState(false);
+
+  const data = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+  
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div>
+    <>
       <h2 style={{ fontWeight: 700, textAlign: "center" }}>
         WELCOME TO THE ADMIN USERS PAGE
       </h2>
-    </div>
+
+      <div style={{display: 'flex', justifyContent: 'end', padding: 10}}>
+        <Button onClick={() => setVisible(true)} type="primary">Tambah Karyawan</Button>
+      </div>
+
+      <Table columns={columns} dataSource={data} />
+
+      <Modal
+        title="Tambah Karyawan"
+        centered
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        width={1000}
+      >
+         <Form
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+          </Form>
+      </Modal>
+    </>
   );
 };
 
