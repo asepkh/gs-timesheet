ec0a7fb6d406542c8051bb27c441a40ea5eb6b10
rediansyahr <rediansyahr@gmail.com>
2022-05-31 09:48:48 +0700

    fix

---
+++ src/pages/Timesheet/controller.js
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import moment from "moment";
+import { message } from "antd";
+import { useQuery, useMutation } from "react-query";
+
+import { getProject } from "@/services/project";
+import { getWorkLocation } from "@/services/workLocation";
+import { addTimesheet, getTimesheet, removeTimesheet } from "@/services/timesheet";
+
+const useController = () => {
+  const [date, setDate] = useState({
+    custom: false,
+    year: moment().format("YYYY"),
+    month: moment().format("M"),
+  });
+  const [modal, setModal] = useState({ visible: false, date: null });
+  const dateFormat = date?.year + "-" + date?.month;
+
+  const { data: companyList } = useQuery("projectAll", () => getProject(), {
+      refetchOnWindowFocus: false,
+    }),
+    { data: workLocationList } = useQuery("workLocationsAll", () => getWorkLocation(), {
+      refetchOnWindowFocus: false,
+    });
+
+  const {
+    data: timesheet,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+    refetch,
+  } = useQuery(["timesheet", { date: dateFormat }], () => getTimesheet({ date: dateFormat }), {
+    refetchOnWindowFocus: false,
+  });
+
+  const add = useMutation(
+    (values) => {
+      return addTimesheet(values);
+    },
+    {
+      onSuccess: () => {
+        message.success("Timesheet Saved Successfully");
+        setModal({ ...modal, visible: false });
+        refetch();
+      },
+      onError: (error) => {
+        message.error(error?.response?.data?.errorMessage || "Add Timesheet Failed");
+      },
+    }
+  );
+
+  const remove = useMutation(
+    (id) => {
+      return removeTimesheet(id);
+    },
+    {
+      onSuccess: () => {
+        message.success("Timesheet Removed Successfully");
+        refetch();
+      },
+      onError: (error) => {
+        message.error(error?.response?.data?.errorMessage || "Remove Timesheet Failed");
+      },
+    }
+  );
+
+  const onFinish = (values) => {
+    console.log(values);
+    add.mutate(values);
+  };
+
+  const onRemove = (id) => {
+    if (!id) return;
+    remove.mutate(id);
+  };
+
+  useEffect(() => {
+    if (!isError) return;
+
+    message.error(error);
+  }, [isError, error]);
+
+  useEffect(() => {
+    if (isLoading || isFetching) return;
+
+    message.loading({
+      content: "Loading...",
+      duration: 0.5,
+      key: "loadingTimesheet",
+    });
+  }, [isLoading, isFetching]);
+
+  return { date, setDate, modal, setModal, dateFormat, timesheet, companyList, workLocationList, onFinish, onRemove };
+};
+
+export default useController;
--- src/pages/Timesheet/index.js
+++ src/pages/Timesheet/index.js
@@ -1,113 +1,15 @@
 import moment from "moment";
-import { useEffect, useState /*, useEffect*/ } from "react";
+import useController from "./controller";
 
-import { useQuery, useMutation } from "react-query";
-import { getProject } from "@/services/project";
-import { getWorkLocation } from "@/services/workLocation";
-import {
-  addTimesheet,
-  getTimesheet,
-  removeTimesheet,
-} from "@/services/timesheet";
-
-import { Card, Row, Select, message } from "antd";
+// Component imports
+import { Card, Row, Select } from "antd";
 import Calendar from "@/components/Calendar";
 import ModalTimesheet from "@/components/ModalTimesheet";
 
 const { Option } = Select;
 const Timesheet = () => {
-  const [custom, setCustom] = useState(false);
-  const [date, setDate] = useState({
-    year: moment().format("YYYY"),
-    month: moment().format("M"),
-  });
-  const [modal, setModal] = useState({ visible: false, date: null });
-  const dateFormat = date?.year + "-" + date?.month;
-
-  const { data: companyList } = useQuery("projectAll", () => getProject(), {
-      refetchOnWindowFocus: false,
-    }),
-    { data: workLocationList } = useQuery(
-      "workLocationsAll",
-      () => getWorkLocation(),
-      {
-        refetchOnWindowFocus: false,
-      }
-    );
-
-  const {
-    data: timesheet,
-    isLoading,
-    isFetching,
-    isError,
-    error,
-    refetch,
-  } = useQuery(
-    ["timesheet", { date: dateFormat }],
-    () => getTimesheet({ date: dateFormat }),
-    { refetchOnWindowFocus: false }
-  );
-
-  const add = useMutation(
-    (values) => {
-      return addTimesheet(values);
-    },
-    {
-      onSuccess: () => {
-        message.success("Timesheet Saved Successfully");
-        setModal({ ...modal, visible: false });
-        refetch();
-      },
-      onError: (error) => {
-        message.error(
-          error?.response?.data?.errorMessage || "Add Timesheet Failed"
-        );
-      },
-    }
-  );
-
-  const remove = useMutation(
-    (id) => {
-      return removeTimesheet(id);
-    },
-    {
-      onSuccess: () => {
-        message.success("Timesheet Removed Successfully");
-        refetch();
-      },
-      onError: (error) => {
-        message.error(
-          error?.response?.data?.errorMessage || "Remove Timesheet Failed"
-        );
-      },
-    }
-  );
-
-  const onFinish = (values) => {
-    console.log(values);
-    add.mutate(values);
-  };
-
-  const onRemove = (id) => {
-    if (!id) return;
-    remove.mutate(id);
-  };
-
-  useEffect(() => {
-    if (!isError) return;
-
-    message.error(error);
-  }, [isError, error]);
-
-  useEffect(() => {
-    if (isLoading || isFetching) return;
-
-    message.loading({
-      content: "Loading...",
-      duration: 0.5,
-      key: "loadingTimesheet",
-    });
-  }, [isLoading, isFetching]);
+  const { date, setDate, modal, setModal, dateFormat, timesheet, companyList, workLocationList, onFinish, onRemove } =
+    useController();
 
   return (
     <Card>
@@ -116,31 +18,24 @@ const Timesheet = () => {
         <div style={{ display: "flex" }}>
           <Select
             style={{ width: 150, marginRight: 10 }}
-            value={custom ? "custom" : moment(dateFormat).format("YYYY-MM")}
+            value={date?.custom ? "custom" : moment(dateFormat).format("YYYY-MM")}
             onChange={(value) => {
-              if (value === "custom") setCustom(true);
-              else {
-                setCustom(false);
+              if (value === "custom") setDate({ ...date, custom: true });
+              else
                 setDate({
-                  month: moment(value).format("M"),
+                  ...date,
+                  custom: false,
                   year: moment(value).format("YYYY"),
+                  month: moment(value).format("M"),
                 });
-              }
             }}
           >
-            <Option
-              value={moment()
-                .subtract(1, "months")
-                .endOf("month")
-                .format("YYYY-MM")}
-            >
-              Bulan terakhir
-            </Option>
+            <Option value={moment().subtract(1, "months").endOf("month").format("YYYY-MM")}>Bulan terakhir</Option>
             <Option value={moment().format("YYYY-MM")}>Bulan ini</Option>
             <Option value="custom">Pilih Sendiri</Option>
           </Select>
           <Select
-            disabled={!custom}
+            disabled={!date?.custom}
             style={{ width: 120, marginRight: 10 }}
             value={date?.month}
             onChange={(value) => setDate({ ...date, month: value })}
@@ -151,22 +46,12 @@ const Timesheet = () => {
               </Option>
             ))}
           </Select>
-          <Select
-            disabled={!custom}
-            value={date?.year}
-            onChange={(value) => setDate({ ...date, year: value })}
-          >
-            <Option
-              value={moment().subtract(1, "year").endOf("year").format("YYYY")}
-            >
+          <Select disabled={!date?.custom} value={date?.year} onChange={(value) => setDate({ ...date, year: value })}>
+            <Option value={moment().subtract(1, "year").endOf("year").format("YYYY")}>
               {moment().subtract(1, "year").endOf("year").format("YYYY")}
             </Option>
-            <Option value={moment().format("YYYY")}>
-              {moment().format("YYYY")}
-            </Option>
-            <Option
-              value={moment().add(1, "year").endOf("year").format("YYYY")}
-            >
+            <Option value={moment().format("YYYY")}>{moment().format("YYYY")}</Option>
+            <Option value={moment().add(1, "year").endOf("year").format("YYYY")}>
               {moment().add(1, "year").endOf("year").format("YYYY")}
             </Option>
           </Select>
