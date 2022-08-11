7e55f5bc8b0a05dc37d42b38281923ca66e3a18d
asepkh <asep.khair@gmail.com>
2022-08-11 17:49:35 +0700

    try pwa

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import "./index.less";
 
 import { QueryClientProvider, QueryClient } from "react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from "@/pages/App";
 
@@ -27,6 +29,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
